a6572fc18cf954fcc63cd8f60fc6fa60580592ee
Fyrreira <117011520+Fyrreira@users.noreply.github.com>
2023-04-29 23:55:11 +0300

    Add files via upload
    Added ring buffer
    Code formated

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -28,12 +28,15 @@
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 uint8_t RxData[256] = {0};
-uint8_t char1[3] = "rck";
-uint8_t char2[6] = "рус";
+uint8_t TxData[256] = {0};
+uint8_t Key_word_1[3] = "rck";
+uint8_t Key_word_2[6] = "рус";
+
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define BUF_SIZE 256 
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -51,7 +54,11 @@ UART_HandleTypeDef huart1;
 /* USER CODE BEGIN PV */
 RTC_TimeTypeDef sTime = {0};
 RTC_DateTypeDef DateToUpdate = {0};
-char buff[65] = {0,};
+char buff_for_RTC[65] = {0,};
+
+uint8_t idxIN;
+uint8_t idxOUT;
+char buff_for_RTCer [BUF_SIZE];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -75,49 +82,48 @@ static void MX_RTC_Init(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+    /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-   
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART1_UART_Init();
-  MX_TIM11_Init();
-  MX_RTC_Init();
-  /* USER CODE BEGIN 2 */
-  
-  HAL_UARTEx_ReceiveToIdle_IT(&huart1, RxData, 256); // Ждем информацию от ПК любого размера
-  HAL_TIM_Base_Start_IT(&htim11);
-  
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+    /* USER CODE END 1 */
     
-    //HAL_Delay(1000);
-    /* USER CODE END WHILE */
+    /* MCU Configuration--------------------------------------------------------*/
 
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
+
+    /* USER CODE BEGIN Init */
+
+    /* USER CODE END Init */
+
+    /* Configure the system clock */
+    SystemClock_Config();
+
+    /* USER CODE BEGIN SysInit */
+   
+    /* USER CODE END SysInit */
+
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_USART1_UART_Init();
+    MX_TIM11_Init();
+    MX_RTC_Init();
+    /* USER CODE BEGIN 2 */
+  
+    HAL_UARTEx_ReceiveToIdle_IT(&huart1, RxData, 256); // Ждем информацию от ПК любого размера
+    HAL_TIM_Base_Start_IT(&htim11);
+    idxOUT = idxIN;
+    /* USER CODE END 2 */
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1)
+        {
+      
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
+        }
+        /* USER CODE END 3 */
 }
 
 /**
@@ -126,48 +132,48 @@ int main(void)
   */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+    /** Configure the main internal regulator output voltage
+    */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE|RCC_OSCILLATORTYPE_LSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.LSEState = RCC_LSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 25;
-  RCC_OscInitStruct.PLL.PLLN = 168;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE|RCC_OSCILLATORTYPE_LSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 25;
+    RCC_OscInitStruct.PLL.PLLN = 168;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 4;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+        {
+            Error_Handler();
+        }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+        {
+            Error_Handler();
+        }
 
-  /** Enables the Clock Security System
-  */
-  HAL_RCC_EnableCSS();
+    /** Enables the Clock Security System
+    */
+    HAL_RCC_EnableCSS();
 }
 
 /**
@@ -178,30 +184,30 @@ void SystemClock_Config(void)
 static void MX_RTC_Init(void)
 {
 
-  /* USER CODE BEGIN RTC_Init 0 */
+    /* USER CODE BEGIN RTC_Init 0 */
 
-  /* USER CODE END RTC_Init 0 */
+    /* USER CODE END RTC_Init 0 */
 
-  /* USER CODE BEGIN RTC_Init 1 */
+    /* USER CODE BEGIN RTC_Init 1 */
 
-  /* USER CODE END RTC_Init 1 */
+    /* USER CODE END RTC_Init 1 */
 
-  /** Initialize RTC Only
-  */
-  hrtc.Instance = RTC;
-  hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
-  hrtc.Init.AsynchPrediv = 127;
-  hrtc.Init.SynchPrediv = 255;
-  hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
-  hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
-  hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
-  if (HAL_RTC_Init(&hrtc) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN RTC_Init 2 */
+    /** Initialize RTC Only
+    */
+    hrtc.Instance = RTC;
+    hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
+    hrtc.Init.AsynchPrediv = 127;
+    hrtc.Init.SynchPrediv = 255;
+    hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
+    hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+    hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+    if (HAL_RTC_Init(&hrtc) != HAL_OK)
+        {
+            Error_Handler();
+        }
+    /* USER CODE BEGIN RTC_Init 2 */
 
-  /* USER CODE END RTC_Init 2 */
+    /* USER CODE END RTC_Init 2 */
 
 }
 
@@ -213,23 +219,23 @@ static void MX_RTC_Init(void)
 static void MX_TIM11_Init(void)
 {
 
-  /* USER CODE BEGIN TIM11_Init 0 */
+    /* USER CODE BEGIN TIM11_Init 0 */
+    
+    /* USER CODE END TIM11_Init 0 */
+    
+    /* USER CODE BEGIN TIM11_Init 1 */
 
-  /* USER CODE END TIM11_Init 0 */
-
-  /* USER CODE BEGIN TIM11_Init 1 */
-
-  /* USER CODE END TIM11_Init 1 */
-  htim11.Instance = TIM11;
-  htim11.Init.Prescaler = 8399;
-  htim11.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim11.Init.Period = 10000;
-  htim11.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim11.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim11) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /* USER CODE END TIM11_Init 1 */
+    htim11.Instance = TIM11;
+    htim11.Init.Prescaler = 8399;
+    htim11.Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim11.Init.Period = 10000;
+    htim11.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    htim11.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+    if (HAL_TIM_Base_Init(&htim11) != HAL_OK)
+        {
+            Error_Handler();
+        }
   /* USER CODE BEGIN TIM11_Init 2 */
 
   /* USER CODE END TIM11_Init 2 */
@@ -244,28 +250,28 @@ static void MX_TIM11_Init(void)
 static void MX_USART1_UART_Init(void)
 {
 
-  /* USER CODE BEGIN USART1_Init 0 */
+    /* USER CODE BEGIN USART1_Init 0 */
 
-  /* USER CODE END USART1_Init 0 */
+    /* USER CODE END USART1_Init 0 */
 
-  /* USER CODE BEGIN USART1_Init 1 */
+    /* USER CODE BEGIN USART1_Init 1 */
 
-  /* USER CODE END USART1_Init 1 */
-  huart1.Instance = USART1;
-  huart1.Init.BaudRate = 9600;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits = UART_STOPBITS_1;
-  huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART1_Init 2 */
+    /* USER CODE END USART1_Init 1 */
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 9600;
+    huart1.Init.WordLength = UART_WORDLENGTH_8B;
+    huart1.Init.StopBits = UART_STOPBITS_1;
+    huart1.Init.Parity = UART_PARITY_NONE;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart1) != HAL_OK)
+        {
+            Error_Handler();
+        }
+    /* USER CODE BEGIN USART1_Init 2 */
 
-  /* USER CODE END USART1_Init 2 */
+    /* USER CODE END USART1_Init 2 */
 
 }
 
@@ -276,23 +282,23 @@ static void MX_USART1_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LED13_GPIO_Port, LED13_Pin, GPIO_PIN_RESET);
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(LED13_GPIO_Port, LED13_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : LED13_Pin */
-  GPIO_InitStruct.Pin = LED13_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED13_GPIO_Port, &GPIO_InitStruct);
+    /*Configure GPIO pin : LED13_Pin */
+    GPIO_InitStruct.Pin = LED13_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(LED13_GPIO_Port, &GPIO_InitStruct);
 
 }
 
@@ -302,39 +308,47 @@ static void MX_GPIO_Init(void)
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) 
     {
         if(huart == &huart1)
-        {
-           if(RxData[0] == char1[0] && RxData[1] == char1[1] && RxData[2] == char1[2])
-           {
-               HAL_UART_Transmit(&huart1, (uint8_t*) "\n\r Never gonna give you up", 28, 1000); 
-           }
-           else if (RxData[0] == char2[0] && RxData[1] == char2[1] && RxData[2] == char2[2])
-               {//1 символ на кириллице = 2 байта
-               HAL_UART_Transmit(&huart1, (uint8_t*) "\n\r Выбор это иллюзия", 36, 2000);
-           }
-           else
-           {
-              HAL_UART_Transmit(&huart1, (uint8_t*) "\n\r Get the following message from usart1: ", 44, 1000);
-              HAL_UART_Transmit_IT(&huart1, RxData, Size);
-           }
-         
-
-        } 
+            {
+            
+                buff_for_RTCer[idxIN++] = *RxData;
+            
+                if(RxData[0] == Key_word_1[0] && RxData[1] == Key_word_1[1] && RxData[2] == Key_word_1[2])
+                    {
+                        HAL_UART_Transmit(&huart1, (uint8_t*) "\n\r Never gonna give you up", 28, 1000); 
+                    }
+                else if (RxData[0] == Key_word_2[0] && RxData[1] == Key_word_2[1] && RxData[2] == Key_word_2[2])
+                    {//1 символ на кириллице = 2 байта
+                        HAL_UART_Transmit(&huart1, (uint8_t*) "\n\r Выбор это иллюзия", 36, 2000);
+                    }
+                else
+                    {
+                        HAL_UART_Transmit(&huart1, (uint8_t*) "\n\r Get the following message from usart1: ", 44, 1000);
+                        HAL_UART_Transmit_IT(&huart1, RxData, Size);
+                    }       
+            } 
         
         HAL_UARTEx_ReceiveToIdle_IT(&huart1, RxData, 256);
     
     }
-    void TIM1_TRG_COM_TIM11_IRQHandler(void)
+    
+    
+void TIM1_TRG_COM_TIM11_IRQHandler(void)
 {
-  /* USER CODE BEGIN TIM1_TRG_COM_TIM11_IRQn 0 */
-
-  /* USER CODE END TIM1_TRG_COM_TIM11_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim11);
-  /* USER CODE BEGIN TIM1_TRG_COM_TIM11_IRQn 1 */
+    /* USER CODE BEGIN TIM1_TRG_COM_TIM11_IRQn 0 */
+    
+    /* USER CODE END TIM1_TRG_COM_TIM11_IRQn 0 */
+    HAL_TIM_IRQHandler(&htim11);
+    /* USER CODE BEGIN TIM1_TRG_COM_TIM11_IRQn 1 */
     HAL_GPIO_TogglePin(LED13_GPIO_Port, LED13_Pin);
     HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BIN); 
-    snprintf(buff, 65, "\n\rTime %d:%d:%d", sTime.Hours, sTime.Minutes, sTime.Seconds);
-    HAL_UART_Transmit(&huart1, (uint8_t*)buff, strlen(buff), 1000);
+    snprintf(buff_for_RTC, 65, "\n\rTime %d:%d:%d", sTime.Hours, sTime.Minutes, sTime.Seconds);
+    HAL_UART_Transmit(&huart1, (uint8_t*)buff_for_RTC, strlen(buff_for_RTC), 1000);
     HAL_RTC_GetDate(&hrtc, &DateToUpdate, RTC_FORMAT_BIN);//Unlock time count    
+    if (idxIN != idxOUT)
+        {
+            *TxData = buff_for_RTCer[idxOUT++];
+            HAL_UART_Transmit_IT(&huart1, TxData, sizeof(TxData));
+        }
   /* USER CODE END TIM1_TRG_COM_TIM11_IRQn 1 */
 }
 /* USER CODE END 4 */
@@ -345,12 +359,12 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1)
+        {
+        }
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -364,9 +378,9 @@ void Error_Handler(void)
   */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line number,
+        ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
